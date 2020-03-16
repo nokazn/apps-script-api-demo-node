3179b3bf30fcabb531d2b1f7e54ed85f558c2aa9
nokazn <baleariclemon@gmail.com>
2020-03-16 11:15:27 +0000

    update SCOPES

--- src/variables.ts
+++ src/variables.ts
@@ -1,6 +1,7 @@
 export const SCOPES = [
   'https://www.googleapis.com/auth/script.projects',
   'https://www.googleapis.com/auth/spreadsheets',
+  'https://www.googleapis.com/auth/drive',
 ] as const;
 
 export const TOKEN_PATH = './token.json' as const;
